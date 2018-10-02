b6675402c1be921353bfb08cfccb6a866846b30a
ALIAKSANDR <aliaksandr.hvozdzeu@gmail.com>
2018-10-02 21:41:23 +0300

    Refactoring, add method for check CV-Code

--- src/main/java/by/htp/hvozdzeu/dao/impl/BalanceBankAccountDAOImpl.java
+++ src/main/java/by/htp/hvozdzeu/dao/impl/BalanceBankAccountDAOImpl.java
@@ -16,7 +16,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static by.htp.hvozdzeu.dao.util.DeleteToken.deleteToken;
 import static by.htp.hvozdzeu.dao.util.ResponseBuilder.buildResponse;
 import static by.htp.hvozdzeu.util.ConstantRool.APP_SECRET_CODE;
 import static by.htp.hvozdzeu.util.Decoder.decrypt;
@@ -31,6 +30,8 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
 
     private static final String SQL_GET_BALANCE_BY_CARD_NUMBER = "SELECT * FROM `bankaccount` WHERE `bankaccount`.`CardNumber` = ?;";
     private static final String SQL_UPDATE_BALANCE = "UPDATE `bankservice`.`bankaccount` SET `BalanceBankAccount`= ? WHERE `CardNumber`= ? AND `CVCode` = ?;";
+    private static final String SQL_CHECK_CV_CODE = "SELECT * FROM bankaccount WHERE bankaccount.CardNumber = ? AND bankaccount.CVCode = ?;";
+    private static final String ERROR_SQL_CHECK_CV_CODE = "Error checking cv-Code.";
     private static final String ERROR_SQL_GET_BALANCE_BY_CARD_NUMBER = "Error getting balance by card number.";
     private static final String ERROR_SQL_UPDATE_BALANCE = "Error write-off balance from credit card.";
     private static final String MIN_CHECK_AMOUNT = "1.00";
@@ -38,6 +39,7 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
     private static final String MSG_STATUS_RESPONSE_SUCCESSFUL_TRANSACTION = "Operation was successful.";
     private static final String MSG_STATUS_RESPONSE_UNSUCCESSFUL_TRANSACTION = "Operation wasn't successful.";
     private static final String MSG_STATUS_RESPONSE_NOT_ENOUGH_MONEY = "To conduct a transaction is impossible, not enough money.";
+    private static final String MSG_STATUS_RESPONSE_NOT_CORRECT_CV_CODE = "Transaction has been canceled duo to not correct cv-code.";
 
     private AuthDAOImpl authDAO = DAOFactory.getAuthDAOImpl();
 
@@ -53,11 +55,10 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
     @Override
     public BalanceAccount balanceBankAccount(String tokenRest, String cardNumber, String appSecretCode) throws DAOException {
         BalanceAccount balanceAccount = null;
-        String secretKey = getSecretKey();
         if (authDAO.findToken(tokenRest) && appSecretCode.equals(APP_SECRET_CODE)) {
             Connection connection = dataBaseConnection.getConnection();
             try (PreparedStatement preparedStatement = connection.prepareStatement(SQL_GET_BALANCE_BY_CARD_NUMBER)) {
-                preparedStatement.setString(1, decrypt(cardNumber, secretKey));
+                preparedStatement.setString(1, decrypt(cardNumber, getSecretKey()));
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     if (resultSet.next()) {
                         balanceAccount = buildBalanceAccountRowMapper(resultSet);
@@ -69,7 +70,7 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
                 dataBaseConnection.closeConnection(connection);
             }
         }
-        deleteToken(tokenRest);
+        //deleteToken(tokenRest);
         return balanceAccount;
     }
 
@@ -90,8 +91,7 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
                                                String cvCode, String appSecretCode) throws DAOException, SQLException {
         boolean status = false;
         String message = null;
-        String secretKey = getSecretKey();
-        if (authDAO.findToken(tokenRest) && appSecretCode.equals(APP_SECRET_CODE)) {
+        if (checkCVCode(cardNumber, cvCode) && authDAO.findToken(tokenRest) && appSecretCode.equals(APP_SECRET_CODE)) {
             BalanceAccount balanceAccount = balanceBankAccount(tokenRest, cardNumber, appSecretCode);
             BigDecimal currentBalance = balanceAccount.getBalanceBankAccount();
             if (currentBalance.intValue() > amount.intValue()) {
@@ -100,14 +100,16 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
                 try (PreparedStatement preparedStatement = connection.prepareStatement(SQL_UPDATE_BALANCE)) {
                     connection.setAutoCommit(false);
                     preparedStatement.setBigDecimal(1, newAmount);
-                    preparedStatement.setString(2, decrypt(cardNumber, secretKey));
-                    preparedStatement.setString(3, decrypt(cvCode, secretKey));
+                    preparedStatement.setString(2, decrypt(cardNumber, getSecretKey()));
+                    preparedStatement.setString(3, decrypt(cvCode, getSecretKey()));
                     preparedStatement.executeUpdate();
                     connection.commit();
                     status = true;
                     message = MSG_STATUS_RESPONSE_SUCCESSFUL_TRANSACTION;
+                    LOGGER.debug("Amount {} was wrote-off", amount);
                 } catch (SQLException e) {
                     message = MSG_STATUS_RESPONSE_UNSUCCESSFUL_TRANSACTION;
+                    LOGGER.debug("Amount {} wasn't wrote-off duo to create rollback.", amount);
                     connection.rollback();
                     throw new DAOException(ERROR_SQL_UPDATE_BALANCE);
                 } finally {
@@ -115,11 +117,14 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
                 }
             } else {
                 message = MSG_STATUS_RESPONSE_NOT_ENOUGH_MONEY;
+                LOGGER.debug("Transaction has been canceled duo to not enough money.");
             }
+
         } else {
             message = MSG_STATUS_RESPONSE_CANCEL_TOKEN;
+            LOGGER.debug("Transaction has been canceled duo to not auth.");
         }
-        deleteToken(tokenRest);
+        //deleteToken(tokenRest);
         return buildResponse(status, message);
     }
 
@@ -140,7 +145,6 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
                                              String cvCode, String appSecretCode) throws DAOException, SQLException {
         boolean status = false;
         String message = null;
-        String secretKey = getSecretKey();
         if (authDAO.findToken(tokenRest) && appSecretCode.equals(APP_SECRET_CODE)) {
             BalanceAccount balanceAccount = balanceBankAccount(tokenRest, cardNumber, appSecretCode);
             BigDecimal currentBalance = balanceAccount.getBalanceBankAccount();
@@ -149,8 +153,8 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
             try (PreparedStatement preparedStatement = connection.prepareStatement(SQL_UPDATE_BALANCE)) {
                 connection.setAutoCommit(false);
                 preparedStatement.setBigDecimal(1, newAmount);
-                preparedStatement.setString(2, decrypt(cardNumber, secretKey));
-                preparedStatement.setString(3, decrypt(cvCode, secretKey));
+                preparedStatement.setString(2, decrypt(cardNumber, getSecretKey()));
+                preparedStatement.setString(3, decrypt(cvCode, getSecretKey()));
                 preparedStatement.executeUpdate();
                 connection.commit();
                 status = true;
@@ -165,7 +169,7 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
         } else {
             message = MSG_STATUS_RESPONSE_CANCEL_TOKEN;
         }
-        deleteToken(tokenRest);
+        //deleteToken(tokenRest);
         return buildResponse(status, message);
     }
 
@@ -184,7 +188,9 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
                                                String cvCode, String appSecretCode) throws DAOException {
         boolean status = false;
         String message = null;
+        LOGGER.info("Start checking credit card.");
         if (authDAO.findToken(tokenRest) && appSecretCode.equals(APP_SECRET_CODE)) {
+            LOGGER.info("Parameters is not empty");
             try {
                 LOGGER.debug("Write-off amount {} from bank account for checking credit card", MIN_CHECK_AMOUNT);
                 writeOffBalanceBankAccount(tokenRest, cardNumber, BigDecimal.valueOf(1.00), cardNumber, appSecretCode);
@@ -192,6 +198,7 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
                 refillBalanceBankAccount(tokenRest, cardNumber, BigDecimal.valueOf(1.00), cardNumber, appSecretCode);
                 status = true;
                 message = "Credit card has been successful checked.";
+                LOGGER.info("Checked was successful.");
             } catch (Exception e) {
                 status = false;
                 message = "Credit card hasn't been checked.";
@@ -199,8 +206,31 @@ public class BalanceBankAccountDAOImpl extends BalanceAccountRowMapper implement
         } else {
             message = MSG_STATUS_RESPONSE_CANCEL_TOKEN;
         }
-        deleteToken(tokenRest);
+        //deleteToken(tokenRest);
+        LOGGER.info("Send response.");
         return buildResponse(status, message);
+    }
+
+
+    private boolean checkCVCode(String creditCardNumber, String cvCode) throws DAOException, SQLException {
+        boolean result = false;
+        Connection connection = dataBaseConnection.getConnection();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(SQL_CHECK_CV_CODE)) {
+            preparedStatement.setString(1, decrypt(creditCardNumber, getSecretKey()));
+            preparedStatement.setString(2, decrypt(cvCode, getSecretKey()));
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    if (resultSet.getRow() == 1) {
+                        result = true;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DAOException(ERROR_SQL_CHECK_CV_CODE);
+        } finally {
+            dataBaseConnection.closeConnection(connection);
+        }
+        return result;
     }
 
 
